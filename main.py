cfb83d4fe060ae54134d1eac7883a541ee5f9402
daniel <tishakovdanyilgmail.com>
2024-01-30 14:45:38 +0000

    weathe in your city

---
+++ main.py
@@ -0,0 +1,21 @@
+"""weather in city"""
+
+# import python_weather
+# import asyncio
+
+
+# async def getweather(city):
+#     async with python_weather.Client(
+#         unit=python_weather.IMPERIAL
+#     ) as client:
+#         weather = await client.get(city)
+#         f_temp = weather.current.temperature
+#         c_temp = (f_temp - 32) / 9*5
+#         print(f'In {city} {round(c_temp, 2)} C')
+    
+# asyncio.set_event_loop_policy(asyncio.DefaultEventLoopPolicy())
+
+
+# asyncio.run(getweather("Formby"))
+
+
